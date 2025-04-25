b510d9211c845c52ce37b627b0309079be256067
ahmed-kamal91 <nagmstar91@gmail.com>
2025-04-25 17:12:08 +0300

    remove the old structure

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$serverName = 'localhost';
-$userName = 'root';     // enter username
-$password = '1234';         // enter password 
-$dbName = 'PHP_Project';
-
-$connect = mysqli_connect($serverName,$userName,$password,$dbName);
-echo ($connect)? "Connected": "Failed to Connect";
-?>
