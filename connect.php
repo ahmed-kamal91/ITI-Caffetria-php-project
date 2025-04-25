37d329eb4aa990f6c90995858aa0c26cee0a7729
ahmed-kamal91 <nagmstar91@gmail.com>
2025-04-25 17:13:34 +0300

    add the new structure

---
+++ connect.php
@@ -0,0 +1,9 @@
+<?php
+$serverName = 'localhost';
+$userName = 'root';     // enter username
+$password = '1234';         // enter password 
+$dbName = 'PHP_Project';
+
+$connect = mysqli_connect($serverName,$userName,$password,$dbName);
+echo ($connect)? "Connected": "Failed to Connect";
+?>
---
+++ orders/createOrder.php
@@ -0,0 +1,29 @@
+<?php
+?>
+
+<html lang="en">
+<head>
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+</head>
+<body>
+
+<div class="container">
+
+</div>
+    
+</body>
+</html>
+
+
+
+
+<!-- 
+create order
+-----------
+list the orders
+choose from the products added 
+appear in the list notes
+click on it more increase the quntity in the note
+increase or decreas the quantity of the added item in the note
+-->
