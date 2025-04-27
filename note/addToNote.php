5ac0ad6f6ba18495bf8ea0c170b6f8a6ba429417
ahmed-kamal91 <nagmstar91@gmail.com>
2025-04-27 19:32:11 +0300

    add add to waiter note feature to add drink from the menu to the waiter note

--- note/addToNote.php
+++ note/addToNote.php
@@ -1,21 +1,51 @@
 <?php
-include './../connect.php';
+// START SESSION
+session_start(); 
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $drink_id = $_POST['drink_id'];
 
+function handleDrinkNotAvailable($drink_id){
+    include './../connect.php';
     $query = "SELECT available FROM drinks WHERE id = $drink_id LIMIT 1";
     $result = mysqli_query($connect, $query);
     $drink = mysqli_fetch_assoc($result);
-
     if ($drink['available'] == 0) {
-        header("Location: ./../drinks/viewDrinks.php?out_of_stock=1");
+        header("Location: ./../pages/userMainPage.php?out_of_stock=1");
         exit();
     }
-
-    // Add to note logic here...
-   echo '<br>';
-   print_r($_POST);
-    // header("Location: ./../drinks/viewDrinks.php");
-    // exit();
 }
+
+
+function addDrink($drink_id){
+    // id exist
+    if(isset($_SESSION['waiterNote'][$drink_id])){
+        // increase the stock
+        $_SESSION['waiterNote'][$drink_id]['stock'] += 1;
+    }else{
+        //add drink
+        $productInfo = [ 
+            'stock' => 1, 'available' => $_POST['drink_available'], 'name' => $_POST['drink_name'], //<==look here for available
+            'price' => $_POST['drink_price'],'image_path' => $_POST['drink_image']];
+        $_SESSION['waiterNote'][$drink_id] = $productInfo;
+    }
+}
+
+
+?>
+
+
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $drink_id = $_POST['drink_id'];
+
+    // check drink availble
+    handleDrinkNotAvailable($drink_id);
+
+    // add drink to note
+    addDrink($drink_id);
+
+    // redirect to user main page
+    header("location: ./../pages/userMainPage.php");
+    exit();
+}
+?>
